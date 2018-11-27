4a3af743f0dbc8115df8d2bfd3da1b3e0ef4bcd9
Philipp Schafer <mail@philippschafer.ch>
2018-11-27 11:05:04 +0100

    initial nextjs setup

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import styles from './index.scss';
+import { Button } from 'boilerplate-form';
+
+const Index = () => (
+  <div className={styles.App}>
+    <header className={styles.header}>
+      <img src="/static/logo.svg" className={styles.logo} alt="logo" />
+      <p>
+        Here we load the form from another project as dependency
+      </p>
+      <Button />
+    </header>
+  </div>
+)
+
+export default Index
